d767b60ce14d381d1a4f611f5446717c5b95324b
Zach <z61@zeesixtyone.com>
2017-10-01 19:55:14 -0400

    Fix #1

--- upload/library/Z61/ThreadMinChars/Install.php
+++ upload/library/Z61/ThreadMinChars/Install.php
@@ -11,12 +11,26 @@ class Z61_ThreadMinChars_Install
             throw new XenForo_Exception('This add-on requires XenForo 1.3.0 or higher.', true);
         }
 
-        if (!$installedAddon) {
+        $version = is_array($installedAddon) ? $installedAddon['version_id'] : 0;
+
+        if (empty($version)) {
 
             foreach (self::_getAlters() AS $alterSql) {
                 try {
                     $db->query($alterSql);
                 } catch (Zend_Db_Exception $e) {
+                }
+            }
+        }
+        else
+        {
+            if ($version < 1010)
+            {
+                foreach(static::_getUpgrades() as $upgradeSql) {
+                    try {
+                        $db->query($upgradeSql);
+                    } catch(Zend_Db_Exception $e) {
+                    }
                 }
             }
         }
@@ -39,10 +53,22 @@ class Z61_ThreadMinChars_Install
 
         $alters['xf_forum'] = "
             ALTER TABLE `xf_forum`
-                ADD COLUMN `z61_character_min_enable` TINYINT(3) UNSIGNED NOT NULL,
+                ADD COLUMN `z61_character_min_enable` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
                 ADD COLUMN `z61_character_min` INT(11) UNSIGNED NOT NULL DEFAULT 5          
         ";
         return $alters;
+    }
+
+    protected static function _getUpgrades()
+    {
+        $updates = array();
+
+        $updates['xf_forum'] = "
+            ALTER TABLE `xf_forum`
+            CHANGE `z61_character_min_enable` DEFAULT 0
+        ";
+
+        return $updates;
     }
 
 }
